590a2ad333ebd01864cd9bdc08db5b869cb1efe9
gaabrielcamargo <gcamargo.dev@gmail.com>
2022-04-26 20:15:49 -0300

    created global style base

---
+++ src/components/App/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Container } from './style';
+
+export default function App(){
+  return(
+    <Container>
+      <h1>a</h1>
+    </Container>
+  )
+}
---
+++ src/components/App/style.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+export const Container = styled.main`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: red;
+  width: 70vw;
+  height: 100vh;
+  margin: 0 auto;
+
+  @media(max-width: 600px){
+    width: 100vw;
+  } 
+`
